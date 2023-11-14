27bc605950a197f728724a6e6a1f32003844299c
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-11-14 17:16:08 -0300

    simulação do EmoSupport funcionando

--- service.py
+++ service.py
@@ -4,6 +4,7 @@ import json
 import secrets
 import colored
 import random
+import time
 from datetime import datetime
 
 FOTOS_VISITANTES = [
@@ -14,6 +15,7 @@ ARQUIVO_DE_CONFIGURACAO = "config.json"
 
 TEMPO_DE_DETECCAO_DE_PACIENTES = 40
 TEMPO_DE_SIMULACAO = 1000
+PROBABILIDADE_ANGUSTIA_DESESPERO = 0.4
 
 def preparar():
     global configuracao
@@ -83,32 +85,94 @@ def reconhecer_pacientes(visitantes):
     if len(pacientes) > 0:
         return True, pacientes
     else:
-        print(colored.fg('black'), colored.bg('red'), "Nenhum paciente reconhecido dentre os visitantes", 
+        print(colored.fg('black'), colored.bg('yellow'), "Nenhum paciente reconhecido dentre os visitantes", 
               colored.attr('reset'))
         return False, None
 
 
 def oferecer_suporte_emocional(paciente):
-    print(colored.fg('black'), colored.bg('green'), f"Oferecendo suporte emocional personalizado para o(a) :{paciente['nome']}", 
+    print(colored.fg('black'), colored.bg('blue'), f"Oferecendo suporte emocional personalizado para o(a) :{paciente['nome']}", 
           colored.attr('reset'))
 
-    # Simulando uma lista de necessidades emocionais do paciente
-    necessidades_emocionais = ["Motivação", "Calma", "Felicidade", "Conforto"]
-    
-    # Sorteando uma necessidade emocional aleatória
-    necessidade_simuladas = random.choice(necessidades_emocionais)
+    while True:
+        if not detectar_sinais_de_angustia_e_desespero(paciente):
 
-    print(colored.fg('black'), colored.bg('white'), f"Necessidade emocional: {necessidade_aleatoria}", colored.attr('reset'))
+            if avaliar_encaminhamento_profissional(paciente):
+                break
 
-    # Lógica para oferecer suporte emocional com base na necessidade emocional sorteada
-    if necessidade_simuladas == "Motivação":
-        print(colored.fg('black'), colored.bg('white'), "Frases motivacionais:")
-        for frase in paciente["suporte_emocional"]["atividades_motivacionais"]:
-            print(colored.fg('black'), colored.bg('white'), f"- {frase}", colored.attr('reset'))
-    elif necessidade_aleatoria == "Calma":
-        print(colored.fg('black'), colored.bg('white'), "Atividades para acalmar:")
-        for atividade in paciente["suporte_emocional"]["atividades_motivacionais"]:
-            print(colored.fg('black'), colored.bg('white'), f"- {atividade}", colored.attr('reset'))
+            # Simulando uma lista de necessidades emocionais do paciente
+            necessidades_emocionais = ["Motivação", "Calma", "Felicidade", "Conforto"]
+            
+            # Sorteando uma necessidade emocional aleatória
+            necessidade_simulada = random.choice(necessidades_emocionais)
+
+            print(colored.fg('black'), colored.bg('white'), f"Necessidade emocional: {necessidade_simulada}", colored.attr('reset'))
+
+            # Lógica para oferecer suporte emocional com base na necessidade emocional sorteada
+            if necessidade_simulada == "Motivação":
+                print(colored.fg('black'), colored.bg('white'), "Frases motivacionais:")
+                frase = paciente["suporte_emocional"]["motivacao"]
+                print(colored.fg('black'), colored.bg('white'), f"- {frase}", colored.attr('reset'))
+            elif necessidade_simulada == "Calma":
+                print(colored.fg('black'), colored.bg('white'), "Atividades para acalmar:")
+                atividade = paciente["suporte_emocional"]["calma"]
+                print(colored.fg('black'), colored.bg('white'), f"- {atividade}", colored.attr('reset'))
+            elif necessidade_simulada == "Felicidade":
+                print(colored.fg('black'), colored.bg('white'), "Atividades para se sentir Feliz:")
+                atividade = paciente["suporte_emocional"]["felicidade"]
+                print(colored.fg('black'), colored.bg('white'), f"- {atividade}", colored.attr('reset'))
+            elif necessidade_simulada == "Conforto":
+                print(colored.fg('black'), colored.bg('white'), "Atividades Confortantes:")
+                atividade = paciente["suporte_emocional"]["conforto"]
+                print(colored.fg('black'), colored.bg('white'), f"- {atividade}", colored.attr('reset'))
+        else:
+            print("Angústia ou desespero detectado. O suporte emocional foi interrompido.")
+            break 
+
+        time.sleep(5)
+
+
+def detectar_sinais_de_angustia_e_desespero(paciente):
+    emocao_detectada = random.random() < PROBABILIDADE_ANGUSTIA_DESESPERO
+
+    if emocao_detectada:
+        print(colored.fg('black'), colored.bg('red'), f"Expressão de angústia ou desespero detectada no(a) paciente {paciente['nome']}",
+              colored.attr('reset'))
+        print(colored.fg('black'), colored.bg('red'), "Ligando para a emergência...", colored.attr('reset'))
+        print(colored.fg('black'), colored.bg('red'), f"Enviando um socorro para o endereço: {paciente['endereco']}.",
+              colored.attr('reset'))
+        return True
+    else:
+        print(colored.fg('white'), colored.bg('green'), "Nenhuma expressão de angústia ou desespero foi detectada.")
+        return False
+
+def avaliar_encaminhamento_profissional(paciente):
+    avaliacao_minima = paciente["encaminhamento_profissional"]["avaliacao_minima"]
+
+    pontuacao_atual = random.randint(1, 10)
+
+    if pontuacao_atual >= avaliacao_minima:
+        print(colored.fg('black'), colored.bg('yellow'), f"{paciente['nome']} demonstrou emoções mais sensíveis.",
+              colored.attr('reset'))
+        print(colored.fg('black'), colored.bg('yellow'), f"Paciente {paciente['nome']} precisa de encaminhamento para ajuda profissional.",
+              colored.attr('reset'))
+        paciente["encaminhamento_profissional"]["historico_suporte"].append(
+                {"data": datetime.now(), "avaliacao": pontuacao_atual, "encaminhado": True}
+                )
+        return encaminhar_para_profissional(paciente)
+    else:
+        print(colored.fg('white'), colored.bg('green'), f"Paciente {paciente['nome']} não precisa de encaminhamento para ajuda profissional no momento.",
+              colored.attr('reset'))
+        paciente["encaminhamento_profissional"]["historico_suporte"].append(
+                {"data": datetime.now(), "avaliacao": pontuacao_atual, "encaminhado": False}
+                )
+        return False
+
+def encaminhar_para_profissional(paciente):
+        print(colored.fg('black'), colored.bg('yellow'), "Encaminhando...", colored.attr('reset'))
+        print(colored.fg('black'), colored.bg('yellow'), "Notificando a equipe de apoio...", colored.attr('reset'))
+        print(colored.fg('black'), colored.bg('white'), f"{paciente['nome']} foi encaminhado para ajuda profissional.", colored.attr('reset'))
+        return True
 
 def imprimir_dados_do_paciente(paciente):
     print(colored.fg('black'), colored.bg('grey_54'), f"Paciente reconhecido em {datetime.now()} dentre os visitantes", 
@@ -141,4 +205,4 @@ if __name__ == "__main__":
 
     ambiente_de_simulacao = simpy.Environment()
     ambiente_de_simulacao.process(reconhecer_visitantes(ambiente_de_simulacao))
-    ambiente_de_simulacao.run(until=1000)
+    ambiente_de_simulacao.run(until=500)
